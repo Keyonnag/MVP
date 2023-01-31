dec2e6b5bef5c21ac6021bb71d4b038453feb5e6
Keyonna Garfine <keyonnagarfine@Keyonnas-Air.lan>
2023-01-31 15:44:36 -0600

    hide/show

--- public/app.js
+++ public/app.js
@@ -1,6 +1,10 @@
 let dinoTypeArray = []
 
-
+$('#homeBtn').click(() => { 
+    $("#dino-type-btn-cont").show();
+    $("#addNewDinoType").show();
+    $("#caraousel-container").show();
+});
 
 async function getDinoTypeForDropDown() {
     const div = document.getElementById('dropdown1')
@@ -12,23 +16,33 @@ async function getDinoTypeForDropDown() {
     dinoTypeArray.forEach((e) => {
         const li = document.createElement('li')
         div.append(li)
-
         const a = document.createElement('a')
         a.id = `${e.dino_type_id}`
         a.classList.add("navbar-item")
         a.innerHTML = `${e.type}`
         console.log(a)
         a.addEventListener('click', (event) => {
+            hideHomePage()
         })
         li.append(a)
     });
 };
 
+getDinoTypeForDropDowns();
+
+async function createCardList(){
+
+}
+function hideHomePage() {
+    $("#dino-type-btn-cont").hide();
+    $("#addNewDinoType").hide();
+    $("#caraousel-container").hide();
+  };
 
 
 
-
-getDinoTypeForDropDown();
+// async function carousolGetDinoList(){}
+// async function createDinoType(){}
 
 
 // const url = `https://ark-tracker.onrender.com/dino_by_type/${a.id}`
