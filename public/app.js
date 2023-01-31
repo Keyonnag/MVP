101557616f646c4a8977fa08bb7afecb0eda14fd
Keyonna Garfine <keyonnagarfine@Keyonnas-Air.lan>
2023-01-31 12:14:00 -0600

    test

--- public/app.js
+++ public/app.js
@@ -1,91 +1,25 @@
 let dinoTypeArray = []
 
-function dropdowns() {
-    let dropdowns = document.querySelectorAll('#dropDown');
 
-    if (dropdowns.length > 0) {
-        dropdowns.forEach(function (el) {
-            el.addEventListener('click', function (event) {
-                event.stopPropagation();
-
-                closeDropdowns(); // <== HERE
-                el.classList.toggle('is-active');
-            });
-        });
-
-        document.addEventListener('click', function (event) {
-            closeDropdowns();
-        });
-    }
-
-    function closeDropdowns() {
-        dropdowns.forEach(function (el) {
-            el.classList.remove('is-active');
-        });
-    }
-}
-dropdowns()
-
-document.addEventListener('DOMContentLoaded', () => {
-    // Functions to open and close a modal
-    function openModal($el) {
-      $el.classList.add('is-active');
-    }
-  
-    function closeModal($el) {
-      $el.classList.remove('is-active');
-    }
-  
-    function closeAllModals() {
-      (document.querySelectorAll('.modal') || []).forEach(($modal) => {
-        closeModal($modal);
-      });
-    }
-  
-    // Add a click event on buttons to open a specific modal
-    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
-      const modal = $trigger.dataset.target;
-      const $target = document.getElementById(modal);
-  
-      $trigger.addEventListener('click', () => {
-        openModal($target);
-      });
-    });
-  
-    // Add a click event on various child elements to close the parent modal
-    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
-      const $target = $close.closest('.modal');
-  
-      $close.addEventListener('click', () => {
-        closeModal($target);
-      });
-    });
-  
-    // Add a keyboard event to close all modals
-    document.addEventListener('keydown', (event) => {
-      const e = event || window.event;
-  
-      if (e.keyCode === 27) { // Escape key
-        closeAllModals();
-      }
-    });
-  });
 
 async function getDinoTypeForDropDown() {
-    const div = document.getElementById('dropDown')
+    const div = document.getElementById('dropdown1')
     const url = "https://ark-tracker.onrender.com/dino/types"
     const response = await fetch(url)
     dinoTypeArray = await response.json() 
     console.log(dinoTypeArray)
     dinoTypeArray.forEach((e) => {
+        const li = document.createElement('li')
+        div.append(li)
+
         const a = document.createElement('a')
         a.id = `${e.dino_type_id}`
         a.classList.add("navbar-item")
         a.innerHTML = `${e.type}`
-        div.append(a)
         console.log(a)
         a.addEventListener('click', (event) => {
         })
+        li.append(a)
     });
 };
 
