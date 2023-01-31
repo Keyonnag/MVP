7354c9ba1851f03d00188f08aa4436ce1f8b7ba8
Keyonna Garfine <keyonnagarfine@Keyonnas-Air.lan>
2023-01-31 17:29:52 -0600

    hide/show

--- public/app.js
+++ public/app.js
@@ -15,7 +15,7 @@ async function getDinoTypeForDropDown() {
         a.id = `${e.dino_type_id}`
         a.classList.add("navbar-item", "waves-effect", "waves-teal", "btn-flat")
         a.innerHTML = `${e.type}`
-        a.id.addEventlistner('click', (event) => {
+        a.addEventlistner('click', (event) => {
             $("#dino-type-btn-cont").hide();
             $("#caraousel-container").hide();
         })
